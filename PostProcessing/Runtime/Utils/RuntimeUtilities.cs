0fb11b9238b60fbd99ecc8ede9c9e7c054401baa
MsK` <msk@lywenn.eu.org>
2019-01-04 12:47:19 +0100

    one XRSettings reference removed from builds

--- PostProcessing/Runtime/Utils/RuntimeUtilities.cs
+++ PostProcessing/Runtime/Utils/RuntimeUtilities.cs
@@ -763,8 +763,8 @@ namespace UnityEngine.Rendering.PostProcessing
                 return isSinglePassStereoSelected && Application.isPlaying;
 #elif UNITY_SWITCH
                 return false;
-#elif UNITY_2017_2_OR_NEWER
-                return UnityEngine.XR.XRSettings.eyeTextureDesc.vrUsage == VRTextureUsage.TwoEyes;
+//#elif UNITY_2017_2_OR_NEWER
+//                return UnityEngine.XR.XRSettings.eyeTextureDesc.vrUsage == VRTextureUsage.TwoEyes;
 #else
                 return false;
 #endif
